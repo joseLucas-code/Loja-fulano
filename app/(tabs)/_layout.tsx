fcd06f2cf2bae3bac70417ff9200f6ac09e2637b
joseLucas-code <skyleer05@gmail.com>
2025-03-04 21:49:28 -0300

    feat: create tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,43 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+const iconSize = 24;
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#8b5cf6',
+        tabBarLabelStyle: { fontSize: 14 },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="categories"
+        options={{
+          title: 'Categorias',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="archive" size={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'Sobre mim',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={iconSize} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,45 @@
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import Button from '@/components/Button';
+import { Link } from 'expo-router';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View>
-        <TouchableOpacity onPress={() => console.log('touch')}>
-          <Text>Hello</Text>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View style={{ gap: 10, alignItems: 'center' }}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/logo.png')}
+          resizeMode="contain"
+        />
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Loja de Fulano</Text>
+        <Text style={{ fontSize: 14 }}>
+          Aqui você encontra tudo oque precisa.
+        </Text>
       </View>
+      <Link replace href={'/home'} asChild>
+        <Button>Começar as compras</Button>
+      </Link>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 32,
+  },
+  logo: {
+    maxWidth: 250,
+    maxHeight: 250,
+  },
+});
